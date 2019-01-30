965b4af7f8a62a4e7783d50a6f6ff208d43a1f2b
AlexanderPhan <42187090+AlexanderPhan@users.noreply.github.com>
2019-01-29 20:43:08 -0600

    starting place for the site

--- server.js
+++ server.js
@@ -1,120 +1,35 @@
-// Include Server Dependencies
-// -------------------------------------------------
-// var React = require('react');
-
-var express = require("express");
-var bodyParser = require("body-parser");
-var expressValidator = require('express-validator');
-var cookieParser = require("cookie-parser");
-var flash = require("connect-flash");
-var LocalStrategy = require("passport-local").Strategy;
-var mongoose = require("mongoose");
-// var morgan = require("morgan");
-var passport = require("passport");
-var path = require("path");
-
-var session = require("express-session");
-var controllers = require ('./server/controllers');
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const routes = require('./routes')
 
 
-// Create Instance of Express
-// -------------------------------------------------
-var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Set Up Express App to Handle Data Parsing
-// -------------------------------------------------
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(cookieParser());
+// Define middleware here
+// DONT TOUCH THIS WAS IMPORTANT AF!
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit:'50mb', extended: true }));
 
-// Static Directory
-// -------------------------------------------------
-app.use(express.static(path.join(__dirname,"public")));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// MongoDB Configuration
-// -------------------------------------------------
-mongoose.Promise = global.Promise;
-var mongoURL = process.env.MONGO || "mongodb://localhost/codespiration"
-mongoose.connect(mongoURL);
-var db = mongoose.connection;
-
-db.on("error", function(err) {
-  console.log("Mongoose Error: ", err);
+// Define API routes here
+app.use(routes)
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/chefinately",{ useNewUrlParser: true });
 
-// Express Session
-app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
-}));
 
-// Passport Initialization
-// -------------------------------------------------
-// Francis' part
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Express Validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
-
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-
-// Connect Flash
-app.use(flash());
-
-// Global Vars
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  res.locals.user = req.user || null;
-  next();
-});
-
-// Middleware for Routes
-// -------------------------------------------------
-var users = require('./server/controllers/user.js');
-var routes = require('./server/controllers/index.js');
-var ideaRoutes = require("./server/controllers/idea-api-routes.js");
-var projectRoutes = require("./server/controllers/project-api-routes.js");
-var techRoutes = require("./server/controllers/tech-api-routes.js");
-var userRoutes = require("./server/controllers/user-api-routes.js");
-
-app.use('/', routes);
-app.use('/users', users);
-app.use("/idea", ideaRoutes);
-app.use("/project", projectRoutes);
-app.use("/tech", techRoutes);
-app.use("/user", userRoutes);
-
-// app.use("/", controllers);
-app.use(express.static(path.join(__dirname, 'static')));
-app.get("*", express.static(path.join(__dirname, './public/index.html')));
-
-// Listener
-// -------------------------------------------------
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
